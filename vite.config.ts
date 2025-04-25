bd363b8433c62629d53ecd6b56b9f03df8517668
maycompires <maycompires@hotmail.com>
2025-04-25 18:53:35 -0300

    cancelamento deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: 'https://github.com/maycompires/RAMApp',
+  plugins: [react()],
   resolve: {
     alias: {
       // Add aliases if needed
@@ -13,12 +11,12 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: [
-      'react',
-      'react-dom',
-      'react-router-dom',
-      'leaflet',
-      'react-leaflet',
-      'leaflet-defaulticon-compatibility',
+      "react",
+      "react-dom",
+      "react-router-dom",
+      "leaflet",
+      "react-leaflet",
+      "leaflet-defaulticon-compatibility",
     ],
   },
 });
