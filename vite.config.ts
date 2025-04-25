fea92ac5f4b2f224d10a8411a3a9b6d58a4a9548
maycompires <maycompires@hotmail.com>
2025-04-25 18:44:14 -0300

    teste deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: 'https://github.com/maycompires/RAMApp',
   resolve: {
     alias: {
       // Add aliases if needed
